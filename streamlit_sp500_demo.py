be3af4011fe11b84795edbe73e1bf5fb93553980
Michel S <michel.safars@gmail.com>
2026-02-16 22:17:08 +0100

    Release v2.4.1 - Suppression espace en haut (padding-top: 0)

--- streamlit_sp500_demo.py
+++ streamlit_sp500_demo.py
@@ -358,7 +358,7 @@ def main():
     # CSS pour resserrer l'espacement vertical de la partie principale
     st.markdown("""
     <style>
-    .block-container { padding-top: 0.3rem !important; padding-bottom: 0.3rem !important; }
+    .block-container { padding-top: 0rem !important; padding-bottom: 0.3rem !important; }
     div[data-testid="stMetric"] { padding: 0px !important; margin-bottom: 0px !important; }
     div[data-testid="stMetricValue"] { font-size: 1.3rem !important; }
     h1 { margin: 0px !important; padding: 0px !important; font-size: 1.6rem !important; line-height: 1.2 !important; }
